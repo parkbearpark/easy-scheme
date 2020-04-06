73c87338b1e40560a4d7ae582df5adac860fd797
yuma <yumai6205@gmail.com>
2020-04-06 18:35:20 +0900

    位置情報、アノテーション等の定義

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+/// 位置情報
+/// Loc(4, 6) => 入力文字の5~7文字目
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Loc(usize, usize);
+
+impl Loc {
+    fn merge(&self, other: &Loc) -> Loc {
+        use std::cmp::{max, min};
+        Loc(min(self.0, other.0), max(self.1, other.1))
+    }
+}
+
+/// アノテーション
+/// Loc を持たせる
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Annot<T> {
+    value: T,
+    loc: Loc,
+}
+
+impl<T> Annot<T> {
+    fn new(value: T, loc: Loc) -> Self {
+        Self { value, loc }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum TokenKind {
+    /// [0-9][0-9]*
+    Number(u64),
+    /// +
+    Plus,
+    /// -
+    Minus,
+    /// *
+    Asterisk,
+    /// /
+    Slash,
+    /// (
+    LParen,
+    /// )
+    RParen,
+}
+
+/// TokenKind にアノテーションを付けたものを Token として定義
+type Token = Annot<TokenKind>;
+
+impl Token {
+    fn number(n: u64, loc: Loc) -> Self {
+        Self::new(TokenKind::Number(n), loc)
+    }
+
+    fn plus(loc: Loc) -> Self {
+        Self::new(TokenKind::Plus, loc)
+    }
+
+    fn minus(loc: Loc) -> Self {
+        Self::new(TokenKind::Minus, loc)
+    }
+
+    fn asterisk(loc: Loc) -> Self {
+        Self::new(TokenKind::Asterisk, loc)
+    }
+
+    fn slash(loc: Loc) -> Self {
+        Self::new(TokenKind::Slash, loc)
+    }
+
+    fn lparen(loc: Loc) -> Self {
+        Self::new(TokenKind::LParen, loc)
+    }
+
+    fn rparen(loc: Loc) -> Self {
+        Self::new(TokenKind::RParen, loc)
+    }
+}
